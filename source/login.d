e80fa104f540c0702dc0e76cc3b58d0debef92eb
julian <julian.limburg@gmx.de>
2025-05-10 15:32:19 +0200

    refactor login stuff into login.d

---
+++ source/login.d
@@ -0,0 +1,113 @@
+module login;
+
+import std.conv;
+import std.format;
+import serverino;
+import passwd;
+import passwd.bcrypt;
+
+import sqlite;
+import session;
+
+@endpoint @route!("/sign_up")
+void sign_up(Request request, Output output) {
+  if (request.method == Request.Method.Get) {
+    output.serveFile("public/sign_up.html");
+    return;
+  } else if (request.method == Request.Method.Post){
+    if (!request.post.has("username") || !request.post.has("email") ||
+        !request.post.has("password")) {
+      output.status = 400;
+      output ~= "Missing argument";
+      return;
+    }
+    string email = request.post.read("email");
+    string username = request.post.read("username");
+    string password = request.post.read("password");
+
+	  scope Database db = new Database("test.db", OpenFlags.READWRITE);
+    if (db.query!(int)(db.prepare_bind!(string)("
+      SELECT count(*) 
+      FROM users
+      WHERE email=?
+    ", email))[0][0] > 0) {
+      output.status = 400;
+      output ~= "Email already exists";
+      return;
+    }
+    if (db.query!(int)(db.prepare_bind!(string)("
+      SELECT count(*) 
+      FROM users
+      WHERE username=?
+    ", username))[0][0] > 0) {
+      output.status = 400;
+      output ~= "Username already exists";
+      return;
+    }
+
+    string password_hash = to!(string)(password.crypt(Bcrypt.genSalt()));
+    
+    db.exec(db.prepare_bind!(string, string, string)("
+        INSERT INTO users (email, username, password_hash)
+        VALUES (?, ?, ?)
+      ", email, username, password_hash));
+
+    int user_id = db.query!(int)(db.prepare_bind!(string, string, string)("
+        SELECT user_id
+        FROM users
+        WHERE email=? AND username=? AND password_hash=?
+      ", email, username, password_hash))[0][0];
+
+    Session session = Session(request, output, "test.db");
+    session.save(user_id);
+
+    output.status = 302;
+    output.addHeader("Location", "/");
+    output ~= "Signed up successfully!" ~ "\n" ~ "You are being redirected.";
+    return;
+  }
+  output.status = 405;
+}
+
+@endpoint @route!("/login")
+void login(Request request, Output output){
+  Session session = Session(request, output, "test.db");
+  if (request.method == Request.Method.Get) {
+    output.serveFile("public/login.html");
+    return;
+  } else if (request.method == Request.Method.Post) {
+    if (!request.post.has("email_or_username") ||
+        !request.post.has("password")) {
+      output.status = 400;
+      output ~= "Missing email_or_username or password";
+      return;
+    }
+    string email_or_username = request.post.read("email_or_username");
+    string password = request.post.read("password");
+
+	  scope Database db = new Database("test.db", OpenFlags.READWRITE);
+    auto query_result = db.query!(int, string)(db.prepare_bind!(string, string)("
+      SELECT user_id, password_hash 
+      FROM users
+      WHERE email=? OR username=?
+    ", email_or_username, email_or_username));
+    
+    if (query_result.length > 0) {
+      int user_id = query_result[0][0];
+      string password_hash = query_result[0][1];
+
+      if (password.canCryptTo(password_hash)) {
+        session.save(user_id);
+        output.status = 302;
+        output.addHeader("Location", "/");
+        output ~= "Logged in successfully!" ~ "\n" ~ "You are being redirected.";
+        return;
+      }
+    }
+
+    output.status = 400;
+    output ~= "Wrong email_or_username or password";
+    return;
+  }
+  output.status = 405;
+}
