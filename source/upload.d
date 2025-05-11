f513cc01a4f67a1e74922a1c84dd8e1e1866a4d1
julian <julian.limburg@gmx.de>
2025-05-11 02:01:09 +0200

    upload frontend and basic endpoint

--- source/upload.d
+++ source/upload.d
@@ -1,34 +1,53 @@
 module upload;
+
 import std.file;
 import std.conv;
+import std.array;
+import std.algorithm;
 import serverino;
+import session;
 
 @endpoint @route!("/upload")
 void upload(Request request, Output output) {
-	import std.experimental.logger;
-	import core.stdc.time;
-	time_t unixTime = core.stdc.time.time(null);
+	Session session = Session(request, output, "test.db");
+	int user_id = session.load();
+  if (request.method == Request.Method.Get) {
+		// TODO: Replace this with proper templating
+		import std.experimental.logger;
+		info(user_id);
+		if(user_id >= 0)
+			output.serveFile("public/upload.html");
+		else
+			output ~= readText("public/upload.html").replace("<!-- ERROR -->", "<div class=\"error-container\">Not logged in</div>");
+	} else {
+		
+		if (user_id == -1) {
+			
+		}
+		
+		import std.experimental.logger;
+		import core.stdc.time;
+		time_t unixTime = core.stdc.time.time(null);
 
-	if(!request.form.read("image").isFile()) {
-		output ~= "Not a valid file";
-		return;
+		const Request.FormData fd = request.form.read("image");
+		if(fd.isFile() && (fd.path.endsWith(".png") || fd.path.endsWith(".jpg"))) {
+			info("File ", fd.filename, " uploaded at ", fd.path);
+			
+			// make sure file doesnt override (even if 2 files are uploaded the same second
+			int i = 0;
+			string target;
+			string img = fd.path;
+			do {
+				// TODO: should preserve the file suffix properly here
+				target = "photos/" ~ to!string(unixTime + i++) ~ "." ~ fd.filename[$-3..$];
+			} while (exists(target));
+			
+			img.copy(target);
+			info("copied file to: ", target);
+			
+			output ~= "received :)\n";
+		} else {
+			output ~= "Not a valid file";
+		}
 	}
-	
-	const Request.FormData fd = request.form.read("image");
-	if (fd.isFile) {
-		info("File ", fd.filename, " uploaded at ", fd.path);
-	}
-
-	// Make sure file doesnt override (even if 2 files are uploaded the same second
-	int i = 0;
-	string target;
-	string img = fd.path;
-	do {
-		target = "photos/" ~ to!string(unixTime + i++) ~ "." ~ fd.filename[$-3..$];
-	} while (exists(target));
-
-	img.copy(target);
-	info("Copied file to: ", target);
-	
-	output ~= "received :)\n";
 }
