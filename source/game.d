b5ba95c6cf862be04e5d04ef05b8f2acc3ec3e13
julian <julian.limburg@gmx.de>
2025-05-14 00:50:21 +0200

    photo and coord endpoint

--- source/game.d
+++ source/game.d
@@ -3,6 +3,7 @@ module game;
 import std.stdio;
 import std.format;
 import std.logger;
+import std.conv;
 
 import serverino;
 import mustache;
@@ -19,20 +20,41 @@ alias Request.Method.Get POST;
 void photo(Request request, Output output) {
 	string location = request.get.read("location");
 	try {
-		scope Database db = new Database("test.db");
-		Stmt stmt = db.prepare_bind("SELECT photo_id, path, latitude, longitude
+		scope Database db = new Database(environment["db_filename"]);
+		Stmt stmt = db.prepare_bind("SELECT photo_id, path
       FROM photos WHERE location = ?
       ORDER BY RANDOM() LIMIT 1", location);
 
-		auto rows = db.query!(int, string, double, double)(stmt);
+		auto rows = db.query!(int, string)(stmt);
 		if(rows.length == 0) throw new Exception("Empty result");
 
 		auto row = rows[0];
-		
-		output ~= format("PhotoID: %d<br>Path: %s<br>Coords: (%f,%f)", row[0], row[1], row[2], row[3]);
+		output.serveFile(row[1]);
+		output.addHeader("photo_id", to!string(row[0]));
 	}
 	catch (Exception e) {
 		error("/photo requested for ", location, " had error: ", e);
+		output.status = 500;
+		output ~= "Internal Server Error\n";
+	}
+}
+
+@endpoint @route!(r => (r.path == "/coords" && r.get.has("photo_id")))
+void coords(Request request, Output output) {
+	string id = request.get.read("photo_id");
+	try {
+		scope Database db = new Database(environment["db_filename"]);
+		Stmt stmt = db.prepare_bind("SELECT latitude, longitude
+      FROM photos WHERE photo_id = ?", id);
+
+		auto rows = db.query!(double, double)(stmt);
+		if(rows.length == 0) throw new Exception("Empty result");
+
+		auto row = rows[0];
+		output ~= format(`{"lat": %2.6f, "long": %2.6f}`, row[0], row[1]);
+	}
+	catch (Exception e) {
+		error("/coords requested for ", id, " had error: ", e);
 		output.status = 500;
 		output ~= "Internal Server Error\n";
 	}
