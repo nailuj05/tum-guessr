16528b8c6da1ab0e704846d9b9b3a962e878c367
Bendeguz Bruno Hoffmann <hoffmann.bruno@outlook.com>
2025-05-22 14:28:58 +0200

    fix header context bug

--- source/header.d
+++ source/header.d
@@ -17,17 +17,17 @@ void set_header_context(Mustache.Context context, Request request, Output output
   int user_id = session_load(request, output);
   if (user_id > 0) {
     context.useSection("logged_in");
-  }
 
-  scope(failure) flogger.error("header context failed");
-  scope Database db = new Database(environment["db_filename"], OpenFlags.READONLY);
-  auto row = db.query!(int, int)(db.prepare_bind!(int)("SELECT is_admin, is_trusted FROM users WHERE user_id = ?", user_id))[0];
+		scope(failure) flogger.error("header context failed");
+		scope Database db = new Database(environment["db_filename"], OpenFlags.READONLY);
+		auto row = db.query!(int, int)(db.prepare_bind!(int)("SELECT is_admin, is_trusted FROM users WHERE user_id = ?", user_id))[0];
 
-  if (row[0] == 1) {
-    context.useSection("is_admin");
-    context.useSection("is_trusted");
-  }
-  if (row[1] == 1) {
-    context.useSection("is_trusted");
+		if (row[0] == 1) {
+				context.useSection("is_admin");
+				context.useSection("is_trusted");
+		}
+		if (row[1] == 1) {
+				context.useSection("is_trusted");
+		}
   }
 }
