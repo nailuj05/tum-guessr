acee3936613d13bd7908137663b8efda5a7a10cd
julian <julian.limburg@gmx.de>
2025-05-24 00:49:31 +0200

    guidelines added

--- source/header.d
+++ source/header.d
@@ -13,21 +13,25 @@ import logger;
 
 alias MustacheEngine!(string) Mustache;
 
-void set_header_context(Mustache.Context context, Request request, Output output) {
+int set_header_context(Mustache.Context context, Request request, Output output) {
   int user_id = session_load(request, output);
   if (user_id > 0) {
     context.useSection("logged_in");
 
 		scope(failure) flogger.error("header context failed");
 		scope Database db = new Database(environment["db_filename"], OpenFlags.READONLY);
-		auto row = db.query!(int, int)(db.prepare_bind!(int)("SELECT is_admin, is_trusted FROM users WHERE user_id = ?", user_id))[0];
+		auto rows = db.query!(int, int)(db.prepare_bind!(int)("SELECT is_admin, is_trusted FROM users WHERE user_id = ?", user_id));
 
-		if (row[0] == 1) {
+    if(rows.length >= 1) {
+      auto row = rows[0];
+      if (row[0] == 1) {
 				context.useSection("is_admin");
 				context.useSection("is_trusted");
-		}
-		if (row[1] == 1) {
-				context.useSection("is_trusted");
-		}
+      }
+      if (row[1] == 1) {
+        context.useSection("is_trusted");
+      }
+    }
   }
+  return user_id;
 }
