48e46d6f0e75bc13ec1248302f1fbddf80e53d5b
julian <julian.limburg@gmx.de>
2025-05-25 00:24:02 +0200

    updated profile pages with more info

--- source/profile.d
+++ source/profile.d
@@ -3,6 +3,8 @@ module profile;
 import std.regex;
 import std.algorithm;
 import std.process : environment;
+import std.datetime : SysTime;
+import std.format;
 
 import mustache;
 import serverino;
@@ -62,19 +64,20 @@ void profile_username(Request request, Output output) {
   string username = username_match[1];
 
   scope Database db = new Database(environment["db_filename"], OpenFlags.READONLY);
-  auto query_result = db.query!(int)(db.prepare_bind!(string)("
-    SELECT user_id
+  auto query_result = db.query!(int, int)(db.prepare_bind!(string)("
+    SELECT user_id, sign_up_time
     FROM users
     WHERE username=?
   ", username));
 
-  if (query_result.length < 1) {
+  if (query_result.length < 1 || username == "unknown") {
     output.status = 404;
     output ~= "Profile does not exist";
     return;
   }
 
   int user_id = query_result[0][0];
+  int sign_up_time = query_result[0][1];
 
   int session_user_id = session_load(request, output);
 
@@ -85,5 +88,15 @@ void profile_username(Request request, Output output) {
   if (session_user_id == user_id) {
     mustache_context.useSection("logged_in");
   }
+
+  auto dateTime = SysTime.fromUnixTime(sign_up_time);
+  mustache_context["join_date"] = format("%02d-%02d-%04d", dateTime.day, dateTime.month, dateTime.year);
+
+  auto games_played    = db.query!(int)(db.prepare_bind!(int)("SELECT COUNT(*) FROM games WHERE user_id = ?", user_id))[0][0];
+  auto photos_uploaded = db.query!(int)(db.prepare_bind!(int)("SELECT COUNT(*) FROM photos WHERE uploader_id = ?", user_id))[0][0];
+
+  mustache_context["games_played"] = games_played;
+  mustache_context["photos_uploaded"] = photos_uploaded;
+  
   output ~= mustache.render("profile", mustache_context);
 }
